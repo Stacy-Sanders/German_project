4f14abf78780f443a0d9d66631d402769e37e912
Stacy-Sanders <stacy.sanders081@gmail.com>
2022-08-02 15:29:08 -0400

    added times and responses

--- germanProject.py
+++ germanProject.py
@@ -2,8 +2,22 @@
 # German practice
 
 
-def germanGreeting(time, name):
-    if time == 'morning':
+def germanGreeting():
+    name = input("Hello, what is your name? ")
+    timeOfDay = input(f"Willkommen, {name}. What time is it: morning, afternoon, evening or night? ")
+    if timeOfDay == 'morning':
         print(f'Guten Morgen, {name}')
 
-    elif
+    elif timeOfDay == 'afternoon':
+        print(f'Guten Tag, {name}')
+
+    elif timeOfDay == 'evening':
+        print(f'Guten Abend, {name}')
+
+    elif timeOfDay == 'night':
+        print(f'Gute Nacht, {name}')
+
+    else:
+        print(f'Sprechen sie Deutsch, {name}?')
+
+germanGreeting()
