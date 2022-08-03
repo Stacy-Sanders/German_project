01c8b3729765fc77f360121b0ea243b504860c90
Stacy-Sanders <stacy.sanders081@gmail.com>
2022-08-03 15:04:27 -0400

    'numbers'

--- germanProject.py
+++ germanProject.py
@@ -44,3 +44,34 @@ def germanGender():
 genderAge = germanGender()
 print(genderAge)
 
+
+def germanNumbers():
+    number_to_translate = int(input('Guten Tag! Today we are going to learn our numbers (0-10) in German! What number would you like to learn first? '))
+
+    if number_to_translate == 0:
+        return 'null'
+    elif number_to_translate == 1:
+        return 'eins'
+    elif number_to_translate == 2:
+        return 'zwei'
+    elif number_to_translate == 3:
+        return 'drei'
+    elif number_to_translate == 4:
+        return 'vier'
+    elif number_to_translate == 5:
+        return 'funf - the u should have an umlaut (..) on top'
+    elif number_to_translate == 6:
+        return 'sechs'
+    elif number_to_translate == 7:
+        return 'sieben'
+    elif number_to_translate == 8:
+        return 'aucht'
+    elif number_to_translate == 9:
+        return 'neun'
+    elif number_to_translate == 10:
+        return 'zehn'
+    else:
+        return 'Please enter a number from 0 - 10'
+
+numLearn = germanNumbers()
+print(numLearn)
