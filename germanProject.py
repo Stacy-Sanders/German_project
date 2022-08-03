952b2d43ae9b867ed36fcd0edcefb12e7b0bdfeb
Stacy-Sanders <stacy.sanders081@gmail.com>
2022-08-03 15:13:02 -0400

    shortened variable, need to fix loop

--- germanProject.py
+++ germanProject.py
@@ -46,32 +46,39 @@ print(genderAge)
 
 
 def germanNumbers():
-    number_to_translate = int(input('Guten Tag! Today we are going to learn our numbers (0-10) in German! What number would you like to learn first? '))
+    number_to_translate = input('Guten Tag! Today we are going to learn our numbers (0-10) in German! What number would you like to learn first? Press enter when done. ')
+    num = int(number_to_translate)
 
-    if number_to_translate == 0:
-        return 'null'
-    elif number_to_translate == 1:
-        return 'eins'
-    elif number_to_translate == 2:
-        return 'zwei'
-    elif number_to_translate == 3:
-        return 'drei'
-    elif number_to_translate == 4:
-        return 'vier'
-    elif number_to_translate == 5:
-        return 'funf - the u should have an umlaut (..) on top'
-    elif number_to_translate == 6:
-        return 'sechs'
-    elif number_to_translate == 7:
-        return 'sieben'
-    elif number_to_translate == 8:
-        return 'aucht'
-    elif number_to_translate == 9:
-        return 'neun'
-    elif number_to_translate == 10:
-        return 'zehn'
+    while num != " ":
+
+        if num == 0:
+            return 'null'
+        elif num == 1:
+            return 'eins'
+        elif num == 2:
+            return 'zwei'
+        elif num == 3:
+            return 'drei'
+        elif num == 4:
+            return 'vier'
+        elif num == 5:
+            return 'funf - the u should have an umlaut (..) on top'
+        elif num == 6:
+            return 'sechs'
+        elif num == 7:
+            return 'sieben'
+        elif num == 8:
+            return 'aucht'
+        elif num == 9:
+            return 'neun'
+        elif num == 10:
+            return 'zehn'
+        else:
+            return 'Please enter a number from 0 - 10'
+
     else:
-        return 'Please enter a number from 0 - 10'
+        return ("Danke!")
+
 
 numLearn = germanNumbers()
 print(numLearn)
